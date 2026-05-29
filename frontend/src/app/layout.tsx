54b122ea69b0c5d0e476f3198fe94952227cd7ce
Sifatul Alam Shohan <sifatulalamshohan@Sifatuls-MacBook-Pro.local>
2026-05-29 21:31:21 +0600

    Use working self-hosted GTM script route

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -68,14 +68,14 @@ export default function RootLayout({
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://server.shobaz.com/lWhoor22RMay68n/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+'https://server.shobaz.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-KW6CXCJK');`,
           }}
         />
         {/* GTM noscript fallback */}
         <noscript>
           <iframe
-            src="https://server.shobaz.com/lWhoor22RMay68n/ns.html?id=GTM-KW6CXCJK"
+            src="https://server.shobaz.com/ns.html?id=GTM-KW6CXCJK"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
